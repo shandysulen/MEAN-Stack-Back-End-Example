80fc8b86ab24e0cdcd405de8743df00e862f8e44
Shandy Sulen <shandysulen@ufl.edu>
2017-11-04 21:21:20 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/message', function(req, res) {
+	console.log(req.body);
+	res.status(200);
+})
+
+var server = app.listen(5000, function() {
+	console.log('listening on port', server.address().port)
+})
