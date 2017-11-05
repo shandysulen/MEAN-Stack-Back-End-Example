293aa33a081424974304a03221ba46310f61bc3b
Shandy Sulen <shandysulen@ufl.edu>
2017-11-04 23:43:13 -0400

    Created app.get() function to retrieve all entries in message collection

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
 var app = express();
-var database;
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Message = mongoose.model('Message', {
+	msg: String
+});
 
 /* Middleware */
 app.use(bodyParser.json());
+
 app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Origin", " *");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	next(); //Do it does not freeze the middleware chain
 })
+/* Middleware */
+
+app.get('/api/message', GetMessages);
 
 app.post('/api/message', function(req, res) {
 	console.log(req.body);
-	database.collection('messages').insertOne(req.body);
+	var message = new Message(req.body);
+	message.save();
 	res.status(200);
 })
 
-mongo.connect("mongodb://localhost:27017/test", function(err, db) {
+function GetMessages(req, res)
+{
+	Message.find({}).exec(function(err, result){
+		res.send(result);
+	})
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err, db) {
 	if (!err){
 		console.log("Connected to Mongo");
-		database = db;
 	}
 })
 
