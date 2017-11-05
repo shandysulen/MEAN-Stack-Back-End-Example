c4145f4e4fe67714dae18283832a3bf76e07f1e5
Shandy Sulen <shandysulen@ufl.edu>
2017-11-04 22:00:52 -0400

    Enable CORES so browsers work fine

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+/* Middleware */
 app.use(bodyParser.json());
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next(); //Do it does not freeze the middleware chain
+})
 
 app.post('/api/message', function(req, res) {
 	console.log(req.body);
